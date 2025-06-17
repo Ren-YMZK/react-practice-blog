267a47ad57d15751a58056f11c4e589689174732
Ren Yamazaki <r26763041@gmail.com>
2025-06-18 02:36:40 +0900

    Googleログアウト機能の実装

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link  } from "react-router-dom";
-import Home from './components/Home';
-import CreatePost from './components/CreatePost';
-import Login from './components/Login';
-import Logout from './components/Logout';
-import Navbar from './components/Navbar';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import CreatePost from "./components/CreatePost";
+import Login from "./components/Login";
+import Logout from "./components/Logout";
+import Navbar from "./components/Navbar";
 import { useState } from "react";
-
-
 
 function App() {
   const [isAuth, setIsAuth] = useState(false);
@@ -19,7 +17,10 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/createpost" element={<CreatePost />}></Route>
         <Route path="/login" element={<Login setIsAuth={setIsAuth} />}></Route>
-        <Route path="/logout" element={<Logout />}></Route>
+        <Route
+          path="/logout"
+          element={<Logout setIsAuth={setIsAuth} />}
+        ></Route>
       </Routes>
     </Router>
   );
