6aebe4be090bc44cea34c49bf9d12ae26b3c0dd3
Ren Yamazaki <r26763041@gmail.com>
2025-06-19 04:03:17 +0900

    削除機能の実装

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "./Home.css";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, deleteDoc, getDocs, doc } from "firebase/firestore";
 import { db } from "../firebase";
 
 const Home = () => {
   const [postList, setPostList] = useState([]);
+
   useEffect(() => {
     const getPosts = async () => {
       const data = await getDocs(collection(db, "posts"));
@@ -17,6 +18,11 @@ const Home = () => {
     getPosts();
   }, []);
 
+  const handleDelete = async (id) => {
+    await deleteDoc(doc(db, "posts", id));
+    window.location.href = "/";
+  };
+
   return (
     <div className="homePage">
       {postList.map((post) => {
@@ -28,7 +34,7 @@ const Home = () => {
             <div className="postTextContainer">{post.postsText}</div>
             <div className="nameAndDeleteButton">
               <h3>@{post.author.username}</h3>
-              <button>削除</button>
+              <button onClick={() => handleDelete(post.id)}>削除</button>
             </div>
           </div>
         );
